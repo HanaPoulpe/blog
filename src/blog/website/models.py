c392aed123f77481df5fa4da336054b0accdcf59
HanaPoulpe <hana@hanapoulpe.org>
2024-07-25 19:16:32 +0200

    feat(front): Add site banner

--- src/blog/website/models.py
+++ src/blog/website/models.py
@@ -1,6 +1,28 @@
+from django.db import models
 from wagtail.contrib.settings import models as wg_settings
 
 
 @wg_settings.register_setting
 class BlogSettings(wg_settings.BaseGenericSetting):
-    pass
+    banner = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        help_text="Website banner",
+        related_name="+",
+    )
+    banner_color = models.CharField(
+        max_length=9,
+        null=False,
+        blank=False,
+        default="#000000",
+        help_text="Background color of the banner.",
+    )
+    title_color = models.CharField(
+        max_length=9,
+        null=False,
+        blank=False,
+        default="#FFFFFF",
+        help_text="Color of the site title.",
+    )
