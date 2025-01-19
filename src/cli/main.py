a0391a9c4fd79bc3353fd58739bc5221b89ca4b2
HanaPoulpe <hana@hanapoulpe.org>
2025-01-19 17:35:35 +0100

    fix(uv): Fix cli commands

--- src/cli/main.py
+++ src/cli/main.py
@@ -7,9 +7,10 @@ def main() -> None:
     # Quick fix before moving long term to click
     parser = argparse.ArgumentParser()
     parser.add_argument("--command", choices=dir(commands), required=True)
+    parser.add_argument("args", nargs="*")
 
     args = parser.parse_args()
     command_name = args.command
 
     command = getattr(commands, command_name)
-    command()
+    command(args.args)
